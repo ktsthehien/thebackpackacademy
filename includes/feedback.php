716432779783b961a86644dc545395328cc29fa5
Hien Nguyen <72104238+ktsthehien@users.noreply.github.com>
2021-07-15 23:34:14 -0700

    Add files via upload

---
+++ includes/feedback.php
@@ -0,0 +1,5 @@
+<div class="contact-feedback">
+    <h3>Your Comments Have Been Received!</h3>
+    <p>Thanks for the input!</p>
+    <p>We'll respond via email within 48 hours, if you requested information.</p>
+</div> 
